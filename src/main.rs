13d8cbba29bebce4c0588037d96fe338b1b0614a
Erik Johnston <erikj@matrix.org>
2016-10-06 15:36:45 +0100

    Move macros to separate module

--- src/main.rs
+++ src/main.rs
@@ -49,57 +49,8 @@ task_local! {
     static CONTEXT: RefCell<Option<ConnectionContext>> = RefCell::new(None)
 }
 
-macro_rules! task_log {
-    ($lvl:expr, $($args:tt)+) => {{
-        use CONTEXT;
-
-        let o = CONTEXT.with(move |m| {
-            m.borrow().as_ref().map(|c| c.logger.clone())
-        });
-        if let Some(log) = o {
-            log!($lvl, log, $($args)+)
-        } else {
-            log!($lvl, ::DEFAULT_LOGGER, $($args)+)
-        }
-    }}
-}
-
-macro_rules! task_trace {
-    ($($args:tt)+) => {{
-        task_log!(::slog::Level::Trace, $($args)+);
-    }}
-}
-
-macro_rules! task_debug {
-    ($($args:tt)+) => {{
-        task_log!(::slog::Level::Debug, $($args)+);
-    }}
-}
-
-macro_rules! task_info {
-    ($($args:tt)+) => {{
-        task_log!(::slog::Level::Info, $($args)+);
-    }}
-}
-
-macro_rules! task_warn {
-    ($($args:tt)+) => {{
-        task_log!(::slog::Level::Warn, $($args)+);
-    }}
-}
-
-macro_rules! task_error {
-    ($($args:tt)+) => {{
-        task_log!(::slog::Level::Error, $($args)+);
-    }}
-}
-
-macro_rules! task_crit {
-    ($($args:tt)+) => {{
-        task_log!(::slog::Level::Crit, $($args)+);
-    }}
-}
-
+#[macro_use]
+mod macros;
 mod bridge;
 mod irc;
 mod matrix;
