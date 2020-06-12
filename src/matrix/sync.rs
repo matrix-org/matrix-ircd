c2a7dface894170f74578e864766277eae24a390
VanillaBrooks <brooks@karlik.org>
2020-06-12 04:15:17 -0700

    futures 3.0 for http.rs / sync.rs rebase
    Signed-off-by: VanillaBrooks <brooks@karlik.org>

--- src/matrix/sync.rs
+++ src/matrix/sync.rs
@@ -12,8 +12,8 @@
 // See the License for the specific language governing permissions and
 // limitations under the License.
 
-use futures::stream::Stream;
-use futures::{Async, Future, Poll};
+//use futures::stream::Stream;
+//use futures::{Async, Future, Poll};
 
 use serde_json;
 
@@ -23,18 +23,30 @@ use super::protocol::SyncResponse;
 
 use tokio_core::reactor::Handle;
 
+use std::pin::Pin;
 use url::Url;
 
-use crate::http::{HttpClient, HttpResponseFuture, Request};
+type CompatResponseFut = hyper::client::ResponseFuture;
 
-use slog::*;
+use futures3::prelude::{Future, Stream, TryFuture};
+use futures3::task::Poll;
+use std::task::Context;
+
+use crate::http;
 
 pub struct MatrixSyncClient {
     url: Url,
     access_token: String,
     next_token: Option<String>,
-    http_stream: HttpClient,
-    current_sync: Option<HttpResponseFuture>,
+    http_client: http::ClientWrapper,
+    current_sync: RequestStatus,
+}
+
+use std::boxed::Box;
+enum RequestStatus {
+    MadeRequest(CompatResponseFut),
+    ConcatenatingResponse(Pin<Box<dyn Future<Output = Result<hyper::body::Bytes, hyper::Error>>>>),
+    NoRequest,
 }
 
 impl MatrixSyncClient {
@@ -52,74 +64,95 @@ impl MatrixSyncClient {
             url: base_url.join("/_matrix/client/r0/sync").unwrap(),
             access_token,
             next_token: None,
-            http_stream: HttpClient::new(host.into(), port, tls, handle),
-            current_sync: None,
+            http_client: http::ClientWrapper::new(),
+            current_sync: RequestStatus::NoRequest,
         }
     }
 
-    pub fn poll_sync(&mut self) -> Poll<SyncResponse, io::Error> {
+    pub fn poll_sync(&mut self, cx: &mut Context<'_>) -> Poll<Result<SyncResponse, io::Error>> {
         task_trace!("Polled sync");
         loop {
-            let sync_response: SyncResponse = {
-                let http_stream = &mut self.http_stream;
-
-                let mut current_sync = if let Some(current_sync) = self.current_sync.take() {
-                    current_sync
-                } else {
-                    self.url
-                        .query_pairs_mut()
-                        .clear()
-                        .append_pair("access_token", &self.access_token)
-                        .append_pair("filter", r#"{"presence":{"not_types":["m.presence"]}}"#)
-                        .append_pair("timeout", "30000");
-
-                    if let Some(ref token) = self.next_token {
-                        self.url.query_pairs_mut().append_pair("since", token);
-                    }
-
-                    self.current_sync = Some(http_stream.send_request(Request {
-                        method: "GET",
-                        path: format!("{}?{}", self.url.path(), self.url.query().unwrap_or("")),
-                        headers: vec![],
-                        body: vec![],
-                    }));
+            match &mut self.current_sync {
+                // There is currently no active request to the matrix server, so we make one
+                RequestStatus::NoRequest => {
+                    self.make_request();
                     continue;
-                };
+                }
+                // We have made a request to the server, now we parse the result
+                RequestStatus::MadeRequest(request) => {
+                    let request = Pin::new(request);
+                    // poll the response from the server to get the body
+                    let response = match request.try_poll(cx) {
+                        Poll::Ready(Ok(r)) => r,
+                        Poll::Ready(Err(e)) => {
+                            task_info!("Error doing sync"; "error" => format!("{}", e));
+                            self.current_sync = RequestStatus::NoRequest;
+                            continue;
+                        }
+                        Poll::Pending => return Poll::Pending,
+                    };
 
-                let response = match current_sync.poll() {
-                    Ok(Async::Ready(r)) => r,
-                    Ok(Async::NotReady) => {
-                        self.current_sync = Some(current_sync);
-                        return Ok(Async::NotReady);
+                    // check response code to make sure the response was 200 Ok
+                    if response.status() == hyper::StatusCode::OK {
+                        return Poll::Ready(Err(io::Error::new(
+                            io::ErrorKind::Other,
+                            format!("Sync returned {}", response.status().as_u16()),
+                        )));
                     }
-                    Err(e) => {
-                        task_info!("Error doing sync"; "error" => format!("{}", e));
-                        self.current_sync = None;
-                        continue;
-                    }
-                };
 
-                if response.code != 200 {
-                    return Err(io::Error::new(
-                        io::ErrorKind::Other,
-                        format!("Sync returned {}", response.code),
-                    ));
+                    // transform the body into a future to contatenate the bytes
+                    // Must pin the future to call poll() with futures 3.0
+                    let bytes_fut = hyper::body::to_bytes(response.into_body());
+                    let pin_bytes = Box::pin(bytes_fut);
+
+                    self.current_sync = RequestStatus::ConcatenatingResponse(pin_bytes);
+                    continue;
                 }
 
-                serde_json::from_slice(&response.data).map_err(|e| {
-                    io::Error::new(
-                        io::ErrorKind::Other,
-                        format!("Sync returned invalid JSON: {}", e),
-                    )
-                })?
+                // We have finished making a request and parsing the response, now we parse the
+                // body
+                RequestStatus::ConcatenatingResponse(pin_bytes) => {
+                    let bytes = match pin_bytes.as_mut().poll(cx) {
+                        Poll::Ready(x) => x.unwrap(),
+                        Poll::Pending => return Poll::Pending,
+                    };
+
+                    // Deserialize the body bytes into a json struct
+                    let sync_response: SyncResponse =
+                        serde_json::from_slice(&bytes).map_err(|e| {
+                            io::Error::new(
+                                io::ErrorKind::Other,
+                                format!("Sync returned invalid JSON: {}", e),
+                            )
+                        })?;
+
+                    task_trace!("Got sync response"; "next_token" => sync_response.next_batch.clone());
+                    self.next_token = Some(sync_response.next_batch.clone());
+                    return Poll::Ready(Ok(sync_response));
+                }
             };
-
-            task_trace!("Got sync response"; "next_token" => sync_response.next_batch.clone());
-
-            self.next_token = Some(sync_response.next_batch.clone());
-
-            return Ok(Async::Ready(sync_response));
         }
+    }
+    fn make_request(&mut self) {
+        self.url
+            .query_pairs_mut()
+            .clear()
+            .append_pair("access_token", &self.access_token)
+            .append_pair("filter", r#"{"presence":{"not_types":["m.presence"]}}"#)
+            .append_pair("timeout", "30000");
+
+        if let Some(ref token) = self.next_token {
+            self.url.query_pairs_mut().append_pair("since", token);
+        }
+
+        let request = hyper::Request::builder()
+            .method("GET")
+            .uri(self.url.as_str().parse::<hyper::Uri>().unwrap())
+            .body(hyper::Body::empty())
+            .expect("request could not be built");
+
+        let response = self.http_client.send_request(request);
+        self.current_sync = RequestStatus::MadeRequest(response);
     }
 }
 
@@ -134,14 +167,18 @@ impl std::fmt::Debug for MatrixSyncClient {
 }
 
 impl Stream for MatrixSyncClient {
-    type Item = SyncResponse;
-    type Error = io::Error;
+    type Item = Result<SyncResponse, io::Error>;
 
-    fn poll(&mut self) -> Poll<Option<SyncResponse>, io::Error> {
+    fn poll_next(
+        mut self: Pin<&mut Self>,
+        cx: &mut Context,
+    ) -> futures3::task::Poll<Option<Self::Item>> {
         task_trace!("Matrix Sync Polled");
 
-        let res = futures::try_ready!(self.poll_sync());
-        Ok(Async::Ready(Some(res)))
+        match self.poll_sync(cx) {
+            Poll::Ready(response) => Poll::Ready(Some(response)),
+            Poll::Pending => Poll::Pending,
+        }
     }
 }
 
